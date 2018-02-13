f9e8f5b344bd2f82a6de2bf6f643fdad55497f32
jkue1 <jkue1@mail.csuchico.edu>
2018-02-12 17:58:49 -0800

    first half

--- main.cpp
+++ main.cpp
@@ -6,11 +6,18 @@ using namespace std;
 
 int main()
 {
-  //declare variables
+string first, last; //declare variables
 
-  //get user input
 
-  //tell fortune
+cout << "what is your first name?";
 
+cin >> first;  //get user input
+cout << "what is your last name?";
+cin >> last; 
+
+cout << "Welcome, " <<first[0]<<"."<<last[0]<<"." ", here is your fortune...\n";
+
+int lucky = first.length()+last.length(); 
+cout<<"your lucky number is " <<lucky<<endl;//tell fortune
   return 0;
 }
