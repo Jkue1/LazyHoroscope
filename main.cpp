e996111fa4137e612639c7292f33e0ee9616f5fa
jkue1 <jkue1@mail.csuchico.edu>
2018-02-14 18:18:21 -0800

    alskdjfa

--- main.cpp
+++ main.cpp
@@ -19,5 +19,27 @@ cout << "Welcome, " <<first[0]<<"."<<last[0]<<"." ", here is your fortune...\n";
 
 int lucky = first.length()+last.length(); 
 cout<<"your lucky number is " <<lucky<<endl;//tell fortune
+
+
+
+char frst=first[0];
+
+if (frst == ( 'a', 'e', 'i', 'o', 'u'))
+{cout << "you are destined to be famous!\n";}
+
+else
+
+{cout << "you should keep a low profile\n";}
+
+
+int len=last.length();
+
+if (last[len-1] = ('a' || 'e' || 'i' || 'o' || 'u'))
+{cout << "you have already met your true love.\n";}
+
+cout << "have a good day!\n";
+
+
+
   return 0;
 }
