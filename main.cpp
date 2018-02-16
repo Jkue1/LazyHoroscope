9acf8ec97e49ba70998bbacd7c8ddde14eb52a7c
jkue1 <jkue1@mail.csuchico.edu>
2018-02-15 16:16:21 -0800

    finish

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ cin >> last;
 
 cout << "Welcome, " <<first[0]<<"."<<last[0]<<"." ", here is your fortune...\n";
 
-int lucky = first.length()+last.length(); 
+int lucky = first.length(); 
 cout<<"your lucky number is " <<lucky<<endl;//tell fortune
 
 
