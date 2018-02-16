e09eae3d1dea6fb62396e120f9d48ba135d8e84e
jkue1 <jkue1@mail.csuchico.edu>
2018-02-15 16:07:04 -0800

    finished

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ int main()
 string first, last; //declare variables
 
 
-cout << "what is your first name?";
+cout << "what is your first name?\n";
 
 cin >> first;  //get user input
-cout << "what is your last name?";
+cout << "what is your last name?\n";
 cin >> last; 
 
 cout << "Welcome, " <<first[0]<<"."<<last[0]<<"." ", here is your fortune...\n";
@@ -22,19 +22,20 @@ cout<<"your lucky number is " <<lucky<<endl;//tell fortune
 
 
 
-char frst=first[0];
 
-if (frst == ( 'a', 'e', 'i', 'o', 'u'))
+if (first[0] == 'a'|| first[0] == 'e'|| first[0] == 'i' || first[0] == 'o'|| first[0] == 'u' || first[0] == 'A' || first[0] == 'E' || first[0] == 'I'|| first[0] == 'O' || first[0] == 'U')
+
 {cout << "you are destined to be famous!\n";}
+
 
 else
 
 {cout << "you should keep a low profile\n";}
 
 
-int len=last.length();
-
-if (last[len-1] = ('a' || 'e' || 'i' || 'o' || 'u'))
+int len = last.length();
+char lst = last[len-1];
+if (lst == 'a' || lst == 'e'|| lst == 'i'|| lst == 'o' || lst == 'u' || lst == 'A' || lst == 'E' || lst == 'I' || lst == 'O' || lst == 'U')
 {cout << "you have already met your true love.\n";}
 
 cout << "have a good day!\n";
